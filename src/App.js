8dd6f78e26d3bff8a963310b7691ec58417711ee
Kara <palumbokara@yahoo.com>
2021-08-06 15:48:13 -0600

    fixed bug

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 import Routes from "./routes/Routes";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from "./UserContext";
+import { useState } from "react";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes />
+        <UserContext.Provider value={{ currentUser }}>
+          <Routes />
+        </UserContext.Provider>
       </BrowserRouter>
     </div>
   );
